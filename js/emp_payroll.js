cc09de3bee66e5de9cf46a9e646d7566a4959863
Jagriti <jagriti.agni@gmail.com>
2020-11-20 02:41:57 +0530

    [Jagriti]ADDED: UC9-Method to create employee payroll data object when submit button clicked

--- js/emp_payroll.js
+++ js/emp_payroll.js
@@ -1,3 +1,48 @@
+class EmployeePayrollData {
+    // constructor
+    constructor(...params) {
+        this.name = params[0];
+        this.salary = params[1];
+        this.gender = params[2];
+        this.startDate = params[3];
+        this.department = params[4];
+        this.notes = params[5];
+    }
+    // method
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+            new Date(this.startDate).toLocaleDateString("en-US", options);
+        return "name='" + this.name + "' , salary=" + this.salary
+            + ", gender=" + this.gender + ", startDate=" + empDate + ", department=" + this.department
+            + ", notes " + this.notes;
+    }
+}
+
+function save() {
+    let departmentsSelected = new Array();
+    const name = document.getElementById("name").value;
+    console.log(name);
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    console.log(gender);
+    const departments = document.querySelectorAll('input[name="department"]:checked');
+    const salary = document.querySelector('#salary').value;
+    console.log(salary);
+    const year = document.querySelector("#year").value;
+    const month = document.querySelector("#month").value;
+    const day = document.querySelector("#day").value;
+    const notes = document.querySelector("#notes").value;
+
+    const startDate = new Date(year, month, day);
+    console.log(startDate);
+    for (let department of departments) {
+        departmentsSelected.push(department.value);
+    }
+    console.log(departmentsSelected);
+    let empPayrollData = new EmployeePayrollData(name, salary, gender, startDate, departmentsSelected, notes);
+    console.log(empPayrollData);
+}
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
