5bf86216bd2046fc1664a193e756cc8dabc95289
Jagriti <jagriti.agni@gmail.com>
2020-11-21 00:49:18 +0530

    [Jagriti]ADDED: Eventlisteners when document is loaded to validate name and date

--- js/emp_payroll.js
+++ js/emp_payroll.js
@@ -1,20 +1,11 @@
 class EmployeePayrollData {
-    // constructor
-    constructor(...params) {
-        this.name = params[0];
-        this.salary = params[1];
-        this.gender = params[2];
-        this.startDate = params[3];
-        this.department = params[4];
-        this.notes = params[5];
-    }
-
     // getter and setter method
     get name() { return this._name; }
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if (nameRegex.test(name))
+        if (nameRegex.test(name)) {
             this._name = name;
+        }
         else throw 'Name is Incorrect!';
     }
 
@@ -35,7 +26,9 @@ class EmployeePayrollData {
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
+        console.log('in func to set date ' + startDate);
         if (startDate < Date.now()) {
+            console.log('date valid  ' + Date.now() + " and ---- " + startDate.getTime());
             this._startDate = startDate;
         } else throw 'Date is invalid!';
     }
@@ -72,7 +65,15 @@ function save() {
         for (let department of departments) {
             departmentsSelected.push(department.value);
         }
-        let empPayrollData = new EmployeePayrollData(name, salary, gender, startDate, departmentsSelected, notes);
+        let empPayrollData = new EmployeePayrollData();
+        empPayrollData.name = name;
+        empPayrollData.salary = salary;
+        empPayrollData.gender = gender;
+        console.log('in save --- ' + startDate);
+        empPayrollData.startDate = startDate;
+        empPayrollData.department = departmentsSelected;
+        empPayrollData.notes = notes;
+
         console.log(empPayrollData);
     } catch (e) {
         console.error(e);
@@ -80,9 +81,45 @@ function save() {
 
 }
 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('#name-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+
+    const year = document.querySelector("#year");
+    const month = document.querySelector("#month");
+    const day = document.querySelector("#day");
+    const dateError = document.querySelector('#date-error');
+    let dateElements = [day, month, year];
+    dateElements.forEach(dateField => {
+        dateField.addEventListener('input', function () {
+            try {
+                const startDate = new Date(year.value, month.value, day.value);
+                console.log(startDate);
+                (new EmployeePayrollData()).startDate = startDate;
+                dateError.textContent = "";
+            } catch (e) {
+                dateError.textContent = e;
+            }
+        });
+
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
 });
