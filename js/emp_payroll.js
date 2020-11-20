cfe6fb44b4b1e76838b185154232ff531cb71f31
Jagriti <jagriti.agni@gmail.com>
2020-11-21 01:36:00 +0530

    [Jagriti]ADDED: Method to reset the form by clicking reset button

--- js/emp_payroll.js
+++ js/emp_payroll.js
@@ -149,3 +149,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
         output.textContent = salary.value;
     });
 });
+
+const resetForm = () => {
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+    document.getElementById('salaryOutput').innerHTML = 40000;
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
